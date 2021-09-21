a11c47ab199d460fbcb2041a8015277cde66e54d
Nikos Gavalas <gavalnick@gmail.com>
2021-09-21 10:41:46 +0200

    Add README and screenshot

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple React Dashboard
+
+A simple dashboard for my self-hosted apps and bookmarks.
+Heavily inspired by [SUI](https://github.com/jeroenpardon/sui).
+
+![screenshot](./screenshot.png)
--- app/src/data.js
+++ app/src/data.js
@@ -1,27 +1,27 @@
 import { Notes, Euro, Cloud, AccessAlarm } from '@material-ui/icons';
 
 export const applications = [
-  { name: 'MyApp',  domain: 'myapp.example.com',  url: 'https://myapp.example.com', icon: Cloud },
-  { name: 'MyOtherApp',      domain: 'myotherapp.example.com',  url: 'https://myotherapp.example.com', icon: Euro },
-  { name: 'YetAnotherApp', domain: 'yetanother.example.com',  url: 'https://yetanother.example.com', icon: Notes },
-  { name: 'AaandAnotherOne', domain: 'andanother.example.com',  url: 'https://andanother.example.com', icon: AccessAlarm },
+  { name: 'MyApp',            domain: 'myapp.example.com',       url: 'https://myapp.example.com',      icon: Cloud },
+  { name: 'MyOtherApp',       domain: 'myotherapp.example.com',  url: 'https://myotherapp.example.com', icon: Euro },
+  { name: 'YetAnotherApp',    domain: 'yetanother.example.com',  url: 'https://yetanother.example.com', icon: Notes },
+  { name: 'AaandAnotherOne',  domain: 'andanother.example.com',  url: 'https://andanother.example.com', icon: AccessAlarm },
 ]
 
 export const bookmarks = [
   { title: 'Category 1', collection: [
-    { name: 'Site1', url: 'https://example.com'},
-    { name: 'Site2', url: 'https://example.com'},
+    { name: 'Site1',  url: 'https://example.com'},
+    { name: 'Site2',  url: 'https://example.com'},
   ]},
   { title: 'Category 2', collection: [
-    { name: 'Site3', url: 'https://example.com'},
-    { name: 'Site4', url: 'https://example.com'},
-    { name: 'Site5', url: 'https://example.com'},
+    { name: 'Site3',  url: 'https://example.com'},
+    { name: 'Site4',  url: 'https://example.com'},
+    { name: 'Site5',  url: 'https://example.com'},
   ]},
   { title: 'Category 3', collection: [
-    { name: 'Site6', url: 'https://example.com'},
-    { name: 'Site7', url: 'https://example.com'},
-    { name: 'Site8', url: 'https://example.com'},
-    { name: 'Site9', url: 'https://example.com'},
+    { name: 'Site6',  url: 'https://example.com'},
+    { name: 'Site7',  url: 'https://example.com'},
+    { name: 'Site8',  url: 'https://example.com'},
+    { name: 'Site9',  url: 'https://example.com'},
     { name: 'Site10', url: 'https://example.com'},
   ]},
   { title: 'Category 4', collection: [
