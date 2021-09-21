3b101b21b6cdd9a595657f52773c6059baf137e0
Nikos Gavalas <gavalnick@gmail.com>
2021-09-21 10:08:38 +0200

    Make the example data a bit nicer

--- app/src/data.js
+++ app/src/data.js
@@ -1,34 +1,35 @@
-import { Notes, Euro, Cloud } from '@material-ui/icons';
+import { Notes, Euro, Cloud, AccessAlarm } from '@material-ui/icons';
 
 export const applications = [
-  { name: 'Nextcloud',  domain: 'example.com',  url: 'https://example.com', icon: Cloud },
-  { name: 'MyApp',      domain: 'example.com',  url: 'https://example.com', icon: Euro },
-  { name: 'MyOtherApp', domain: 'example.com',  url: 'https://example.com', icon: Notes },
-  { name: 'MyOtherApp', domain: 'example.com',  url: 'https://example.com', icon: Notes },
+  { name: 'MyApp',  domain: 'myapp.example.com',  url: 'https://myapp.example.com', icon: Cloud },
+  { name: 'MyOtherApp',      domain: 'myotherapp.example.com',  url: 'https://myotherapp.example.com', icon: Euro },
+  { name: 'YetAnotherApp', domain: 'yetanother.example.com',  url: 'https://yetanother.example.com', icon: Notes },
+  { name: 'AaandAnotherOne', domain: 'andanother.example.com',  url: 'https://andanother.example.com', icon: AccessAlarm },
 ]
 
 export const bookmarks = [
-  { title: 'Cat 1', collection: [
-    { name: 'asdf1', url: 'aaa'},
-    { name: 'asdf2', url: 'bbb'},
+  { title: 'Category 1', collection: [
+    { name: 'Site1', url: 'https://example.com'},
+    { name: 'Site2', url: 'https://example.com'},
   ]},
-  { title: 'Cat 2', collection: [
-    { name: 'asdf1', url: 'aaa'},
-    { name: 'asdf3', url: 'bbb'},
+  { title: 'Category 2', collection: [
+    { name: 'Site3', url: 'https://example.com'},
+    { name: 'Site4', url: 'https://example.com'},
+    { name: 'Site5', url: 'https://example.com'},
   ]},
-  { title: 'Cat 3', collection: [
-    { name: 'asdf1', url: 'aaa'},
-    { name: 'asdf2', url: 'bbb'},
-    { name: 'asdf2', url: 'bbb'},
-    { name: 'asdf2', url: 'bbb'},
-    { name: 'asdf2', url: 'bbb'},
+  { title: 'Category 3', collection: [
+    { name: 'Site6', url: 'https://example.com'},
+    { name: 'Site7', url: 'https://example.com'},
+    { name: 'Site8', url: 'https://example.com'},
+    { name: 'Site9', url: 'https://example.com'},
+    { name: 'Site10', url: 'https://example.com'},
   ]},
-  { title: 'Cat 3', collection: [
-    { name: 'asdf1', url: 'aaa'},
-    { name: 'asdf2', url: 'bbb'},
+  { title: 'Category 4', collection: [
+    { name: 'Site11', url: 'https://example.com'},
+    { name: 'Site12', url: 'https://example.com'},
   ]},
-  { title: 'Cat 3', collection: [
-    { name: 'asdf1', url: 'aaa'},
-    { name: 'asdf2', url: 'bbb'},
+  { title: 'Category 5', collection: [
+    { name: 'Site13', url: 'https://example.com'},
+    { name: 'Site14', url: 'https://example.com'},
   ]},
 ]
